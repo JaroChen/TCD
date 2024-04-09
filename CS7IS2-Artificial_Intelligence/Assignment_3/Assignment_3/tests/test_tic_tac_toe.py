e329a663c9485d10a291c800f813c39a8a5aa5be
Jaro <cjingp7@gmail.com>
2024-04-09 23:02:45 +0100

    requirement
    add the package file

--- CS7IS2-Artificial_Intelligence/Assignment_3/Assignment_3/tests/test_tic_tac_toe.py
+++ CS7IS2-Artificial_Intelligence/Assignment_3/Assignment_3/tests/test_tic_tac_toe.py
@@ -11,20 +11,20 @@ class TestTicTacToe(unittest.TestCase):
 
     def test_make_move(self):
         board = initialize_board()
-        make_move(board, 0, 0, 'X')
-        self.assertEqual(board[0][0], 'X', "The top-left corner should be 'X'.")
+        make_move(board, 0, 0, 'A')
+        self.assertEqual(board[0][0], 'A', "The top-left corner should be 'X'.")
 
     def test_winner(self):
         board = initialize_board()
         # Simulate a winning condition
-        board[0] = ['X', 'X', 'X']
-        self.assertTrue(check_winner(board, 'X'), "Player X should be the winner.")
+        board[0] = ['A', 'A', 'A']
+        self.assertTrue(check_winner(board, 'A'), "Player A should be the winner.")
 
     def test_draw(self):
         board = [['X' if (i + j) % 2 == 0 else 'O' for j in range(3)] for i in range(3)]
         board[1][1] = ' '  # Leave one space empty
         self.assertFalse(check_draw(board), "The board is not full, so it shouldn't be a draw.")
-        board[1][1] = 'X'  # Fill the space to simulate a draw
+        board[1][1] = 'A'  # Fill the space to simulate a draw
         self.assertTrue(check_draw(board), "The board is full, so it should be a draw.")
 
 
